b2680ee609e9bfefe71c705e7a97f9c34a822983
Branzil <branzil.nbj@gmail.com>
2022-07-15 19:51:55 +0530

    first

--- todo-cards-pro.js
+++ todo-cards-pro.js
@@ -1,80 +1,87 @@
-let root = document.querySelector('.cards');
-let card = ``;
-let id = 1;
 
+let cards = document.querySelector('.cards');
+let note0 = document.getElementById('notes0')
+// let cardNumber  = 0;
 
-let Update = (id , NoteText) => (
-    ` <div class="card${id} card">
-  <label for="textarea">Note ${id}</label>
-  <textarea name="note" id="notes${id}" class="note" cols="30" rows="10" placeholder="Add your notes here">${NoteText}</textarea>
-  <div class="btns">
-  <button class="btnAdd btn" onclick={addNote()}>Add More</button>
-  <button class="btnDel btn" onclick={deleteNote()} >Delete</button>
-  </div>
-  </div>`
-)
+//basic initial template**************************************
 
-let NotesText = Array(10).fill('');
-
-
-
-class Card {
-    constructor(id, content, val) {
-        this.index = id,
-            this.content = content,
-            this.val = val
-    }
-}
-
-
-let cardData = [new Card(1, Update(1))];
-
-let Notes = document.getElementsByClassName('note');
-
-Array.from(Notes).forEach((element) => {
-    element.addEventListener('keydown', (e) => {
-        let str = e.target.id
-        let idn = str.charAt(str.length-1)
-        NotesText[idn-1]=e.target.value;
-        console.log(idn)
-        console.log(NotesText)
-        // cardData[id-1].val = e.target.value
-    })
+note0.addEventListener('input',(e)=>{
+  localStorage.setItem(`notes0`, e.target.value);
+  note0.innerHTML = e.target.innerHTML
+  localStorage.setItem('cards', cards.innerHTML)
+  localStorage.setItem('cardNumber', cardNumber)
 })
+//********************************************************** */
 
 
-let addNote = () => {
-   if(id<10){ root.innerHTML = '';
-    id++;
-    cardData.splice(id, 0, new Card(id, Update(id) , NotesText[id-2]))
-    cardData.forEach((val) => {
-        if (val !== undefined)
-            root.innerHTML += Update(val.index , NotesText[cardData.indexOf(val)] )
+//to set the page acc to the stored value on referesh***********
+//setting content according to the sotored values on refresing**
+if(localStorage.length)
+cards.innerHTML=localStorage.getItem('cards')
+
+if(localStorage.getItem('cardNumber'))
+cardNumber= localStorage.getItem('cardNumber')
+else
+ cardNumber  = 0;
+
+let noteCards = document.getElementsByClassName('card');
+
+for(let i =0;i<noteCards.length;i++){
+  document.getElementById(`notes${i}`).innerHTML = localStorage.getItem(`notes${i}`);
+}
+//********************************************************* */
+
+
+//clearing local storage on click
+const clearStorage = ()=>{
+  localStorage.clear();
+}
+
+const addNote = ()=>{
+  cardNumber++;
+  cards.innerHTML+= `  <div class="card " id = "${cardNumber}">
+  <label for="textarea" >Note ${cardNumber}</label>
+  <textarea name="note" id="notes${cardNumber}" class="note" cols="30" rows="10" placeholder="Add your notes here"></textarea>
+  <div class="btns">
+  <button class="btnAdd btn" onclick={addNote()} >Add More</button>
+    <button class="btnDel btn" onclick={deleteNote(${cardNumber})} >Delete</button>
+    </div>
+  </div>`;
+
+ noteCards = document.getElementsByClassName('card');
+
+  for(let i =0;i<noteCards.length;i++){
+
+    let note = document.getElementById(`notes${i}`);
+
+    note.addEventListener('input',(e)=>{
+      localStorage.setItem(`notes${i}`, e.target.value);
+      note.innerHTML = e.target.value
+      
     })
-    let Notes = document.getElementsByClassName('note');
-    Array.from(Notes).forEach((element) => {
-        element.addEventListener('keydown', (e) => {
-            let str = e.target.id
-            let idn = str.charAt(str.length-1)
-            NotesText[idn-1]=e.target.value;
-           
-            // cardData[id-1].val = e.target.value
-        })
-    })}
+  }
+  localStorage.setItem('cards', cards.innerHTML)
+  localStorage.setItem('cardNumber', cardNumber)  
+} 
+
+//deleting the cards ****************************************** */
+
+deleteNote = (num) =>{
+ document.getElementById(num).style.display = 'none';
+ localStorage.removeItem(`notes${num}`);
+ noteCards = document.getElementsByClassName('card');
+
+ for(let i =0;i<noteCards.length;i++){
+  
+if(document.getElementById(`notes${i}`))
+ {  note = document.getElementById(`notes${i}`);
+
+   note.addEventListener('input',(e)=>{
+     localStorage.setItem(`notes${i}`, e.target.value);
+     note.innerHTML = e.target.value
+     
+   })}
+ }
+ localStorage.setItem('cards', cards.innerHTML)
+ localStorage.setItem('cardNumber', cardNumber) 
 }
-
-let deleteNote = () => {
-    if( cardData.length>1){
-
-        root.innerHTML = '';
-        id--;
-        delete cardData[id];
-        cardData.forEach((val) => {
-            if (val !== undefined)
-            root.innerHTML += Update(val.index, NotesText[cardData.indexOf(val)])
-        })
-    }
-}
-
-
-
